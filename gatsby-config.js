a1f3c95a4bdd7c18744d9f68e0156754810855d1
Eugene Ahn <01.ahn.eugene@gmail.com>
2021-12-29 13:17:21 -0800

    button change test

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Eugene Exploration site',
   },
   plugins: ['gatsby-plugin-react-helmet'],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,10 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Design Playground</h1>
+    <p>Welcome to Eugene's code and design playground. Here, I will explore and build prototypes to design with code</p>
+   <p>Now let's go and build something great.</p>
+    <Link to="/page-2/">Go to lalaland </Link>
   </div>
 )
 
